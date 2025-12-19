e7a1bb17630d7eb309253c6888112fc006fb607f
BF <bran.figueroa@duocuc.cl>
2025-12-19 15:32:04 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,4 @@
-# ---------------------------------------------------------
-# VARIABLES BÁSICAS
-# ---------------------------------------------------------
+
 variable "aws_region" {
   default = "us-east-1"
 }
@@ -9,9 +7,7 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-# ---------------------------------------------------------
-# MEJORA: PARAMETRIZACIÓN DE ENTORNO
-# ---------------------------------------------------------
+
 variable "project_name" {
   description = "Nombre del proyecto (se usará para etiquetar)"
   type        = string
@@ -24,9 +20,7 @@ variable "environment" {
   default     = "dev"
 }
 
-# ---------------------------------------------------------
-# MEJORA: CONDICIONAL PARA CREAR RECURSOS
-# ---------------------------------------------------------
+
 variable "create_database" {
   description = "Booleano para decidir si crear la BD o no (true/false)"
   type        = bool
@@ -47,3 +41,4 @@ variable "instance_type" {
 variable "instance_count" {
   default = 2
 }
+
